cb4a8a645a410dc08f9f98201323ea3331235392
Tobi <antonipustolka@gmail.com>
2019-07-02 21:48:23 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -65,6 +65,30 @@ class NewVisitorTest(LiveServerTestCase):
        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
+   def test_layout_and_styling(self):
+       # Edith goes to the home page
+       self.browser.get(self.live_server_url)
+       self.browser.set_window_size(1024, 768)
+       
+       # She notices the imput box is nicely centered
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       self.assertAlmostEqual(
+           inputbox.location['x'] + inputbox.size['width'] / 2,
+           512,
+           delta=10
+       )
+       # She starts a new list and sees the imput is nicely
+       # centered there too
+       inputbox.send_keys('testing')
+       inputbox.send_keys(Keys.ENTER)
+       self.wait_for_row_in_list_table('1: testing')
+       inputbox = self.browser.find_element_by_id('id_new_item')
+       self.assertAlmostEqual(
+           inputbox.location['x'] + inputbox.size['width'] / 2,
+           512,
+           delta=10
+       )           
+
        # Satisfied, she goes back to sleep
 
    def test_mulitpe_users_can_start_lists_at_different_urls(self):
